649e58940dd447b135c0217bdae25ac07dee2271
BBatts24 <philipcolborn@gmail.com>
2024-04-04 15:38:10 -0400

    trying to pull

--- src/App.tsx
+++ src/App.tsx
@@ -41,6 +41,8 @@ function App() {
         </a>
       </header>
       <h1>Philip</h1>
+      <h1>Brett Bockstein</h1>
+      <h1>Miles Gaydos</h1>
       <Form>
         <Form.Label>API Key:</Form.Label>
         <Form.Control type="password" placeholder="Insert API Key Here" onChange={changeKey}></Form.Control>
